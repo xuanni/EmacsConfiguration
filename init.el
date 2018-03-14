73e098bd7d11d331b335aef8860287bfd3730558
Sean Ni <sean1ni@gmail.com>
2018-03-13 22:40:37 -0700

    remove init.el customize

--- init.el
+++ init.el
@@ -26,17 +26,3 @@
 ;; Idealy there should be nothing below this line
 ;; as long as there is no `customize.el' used in the configuration
 ;; ----------------------------------------------------------------
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yasnippet window-numbering p4 org-bullets notmuch magit htmlize helm-projectile helm-gtags company))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
