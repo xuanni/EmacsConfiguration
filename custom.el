0288a09ce85c28d3afbccf9616fb09792220ef84
Sean Ni <sean1ni@gmail.com>
2021-03-16 00:09:25 -0700

    Use theme tsdh-light

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(tsdh-light))
  '(package-selected-packages
    '(magit yasnippet-snippets window-numbering which-key wgrep-helm wgrep-ag undo-tree smartparens org-web-tools org-bullets helm-projectile helm-gtags helm-ag exec-path-from-shell elpy dired-narrow delight company-irony-c-headers company-irony captain bm avy auctex)))
 (custom-set-faces
