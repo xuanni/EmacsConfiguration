5f3bf7d53dcef2c1ef660f156cc670960ded7896
Xuan Ni <xni@qti.qualcomm.com>
2021-06-17 14:57:51 -0700

    Add 3 packages: epresent, flycheck-irony (flycheck dep)

---
+++ elpa/flycheck-irony-20180604.2152/flycheck-irony-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from flycheck-irony.el  -*- no-byte-compile: t -*-
+(define-package "flycheck-irony" "20180604.2152" "Flycheck: C/C++ support via Irony" '((emacs "24.1") (flycheck "0.22") (irony "0.2.0")) :commit "42dbecd4a865cabeb301193bb4d660e26ae3befe" :authors '(("Guillaume Papin" . "guillaume.papin@epitech.eu")) :maintainer '("Guillaume Papin" . "guillaume.papin@epitech.eu") :keywords '("convenience" "tools" "c") :url "https://github.com/Sarcasm/flycheck-irony/")
