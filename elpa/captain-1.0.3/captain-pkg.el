2304ea8828155316ce6e09f6f9b49f19aeda67d9
Sean Ni <sean1ni@gmail.com>
2021-10-04 10:03:57 -0700

    [Major change] Change to use-package and remove elpa/ from git

--- elpa/captain-1.0.3/captain-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/sean/.emacs.d/elpa/captain-1.0.3/captain.el  -*- no-byte-compile: t -*-
-(define-package "captain" "1.0.3" "CAPiTalization is Automatic IN emacs" 'nil :url "http://elpa.gnu.org/packages/captain.html" :keywords '("editing") :authors '(("Ian Dunn" . "dunni@gnu.org")) :maintainer '("Ian Dunn" . "dunni@gnu.org"))
